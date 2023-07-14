97e94963ad19506635f08c95a5dbb6a9aa3cbd70
kmivehchi-xRoird <kammivehchi@gmail.com>
2023-07-14 16:06:36 -0700

    add hero images

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './components/home'
 
 function App() {
   return (
-    < div className=" bg-fixed min-h-screen bg-gradient-to-r from-black via-gray-900 to-gray-900 max-w-screen overflow-hidden relative" >
+    < div className=" bg-fixed min-h-screen bg-gradient-to-r from-black via-gray-900 to-gray-900 max-w-screen overflow-hidden relative " >
       <Navigation />
       <Home />
       <Footer />
--- src/components/about.js
+++ src/components/about.js
@@ -10,13 +10,14 @@ const About = () => {
             <div className=" mx-auto  flex flex-col items-center pt-6 pb-14 gap-4 lg:flex-row lg:flex-wrap lg:justify-center ">
                 {/* <Parallax speed={20} translateX={["70%", '20%',]} opacity={[3, -1]} >
                 </Parallax> */}
+
                 <h1 className='text-6xl lg:text-8xl  font-bold   text-teal-500  lg:basis-full text-center '>About</h1>
                 {/* <Parallax speed={0} translateX={['25%', '0',]} >
                 </Parallax> */}
                 <div className="avatar lg:basis-1/3  lg:my-6">
                     <div className="w-48 md:w-72  mx-auto  rounded-full  mask mask-hexagon">
 
-                        <img src={content.about.image.url} alt={content.about.image.alt} className="" />
+                        <img src={content.about.image.url} alt={content.about.image.alt} className="" loading="lazy" />
                     </div>
                 </div>
                 <p className="text-center lg:text-left text-sm md:text-xl  2xl:text-3xl lg:basis-1/2 w-10/12 lg:w-full relative before:content-[''] before:absolute before:bg-teal-500 before:w-full before:h-1 before:left-0 before:top-[-.75rem]    lg:before:h-[120%] lg:before:w-1 lg:before:left-[-1.5rem] lg:before:top-0 xl:before:left-[-2rem]  xl:before:w-2">{content.about.desc}</p>
